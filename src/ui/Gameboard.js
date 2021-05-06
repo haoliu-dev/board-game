3e14dd9a585f6c8fabdfc412610799c15343b1d7
Hao Liu <haoliu-dev@gmail.com>
2021-05-05 20:08:54 -0700

    use flex to layout map

--- src/ui/Gameboard.js
+++ src/ui/Gameboard.js
@@ -7,17 +7,19 @@ export default class Gameboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      map: new HexMap(12, 6),
+      map: new HexMap(20, 16),
     }
   }
   render() {
     const tiles = this.state.map.tiles.map(t =>
       <HextileUI key={t.x + ":" + t.y} hextile={t}></HextileUI>
     );
+    const mapWidthPx = this.state.map.width * 64 + 32;
+    const mapHeightPx = this.state.map.height * 64;
     return (
       <div>
         map created: width = {this.state.map.width}, height = {this.state.map.height}
-        <div className="hextile-container">
+        <div className="hextile-container" style={{ width: mapWidthPx, height: mapHeightPx }}>
           {tiles}
         </div>
       </div>
--- src/ui/HextileUI.js
+++ src/ui/HextileUI.js
@@ -13,9 +13,10 @@ export default class HextileUI extends React.Component {
     const hextile = this.state;
     const top = TILE_WIDTH_PX * hextile.x;
     const left = TILE_HEIGHT_PX * hextile.y + ((hextile.x % 2 === 0) ? 0 : TILE_WIDTH_PX / 2);
+    const firstColClassName = (hextile.y === 0 && hextile.x %2 === 0) ? 'first-col' : '';
     return (
-      <div className={`hextile terrain-${hextile.terrain.name}`} style={{ left: left + "px", top: top + "px" }} onClick={this.updateElevation}>
-        
+      <div className={`hextile ${firstColClassName} terrain-${hextile.terrain.name}`} style={{ left: left + "px", top: top + "px" }} onClick={this.updateElevation}>
+
       </div>
     );
 
@@ -23,6 +24,6 @@ export default class HextileUI extends React.Component {
 
   // event handlers
   updateElevation = () => {
-    this.setState({ elevation: this.state.elevation + 880 });
+    alert(`you clicked a tile of ${this.state.terrain.name}`);
   }
 }
